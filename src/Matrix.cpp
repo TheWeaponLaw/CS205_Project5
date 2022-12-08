be6203c9744e0078b3e6ec980169e14d03e6fd2e
Weapon <447922934@qq.com>
2022-12-08 16:28:30 +0800

    beginning

---
+++ src/Matrix.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/Matrix.hpp"
+
+using namespace std; // memcpy
+
+template <typename T>
+Matrix<T>::Matrix(size_t row, size_t column, size_t channel)
+{
+    cout << "Constructer!" << endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "Matrix.cpp"
+
+int main()
+{
+    Matrix<int> matrix(1, 1, 1);
+}
